f4e09e074353669c41fc4d3d5537f24fdc60f983
italost <italoaraujo.4865@aluno.saojudas.br>
2021-03-08 19:56:08 -0300

    Commit inicial. Classe Empregado criada

---
+++ Empregado.java
@@ -0,0 +1,20 @@
+public class Empregado{
+    private String nome;
+    private int idade;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
