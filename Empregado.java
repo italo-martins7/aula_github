74df20e6e736dc5aed2788efb49314df9f87d0f7
Italo <italost_martins@hotmail.com>
2021-03-08 21:00:53 -0300

    mudancas finais que faltaram rs

--- Empregado.java
+++ Empregado.java
@@ -1,40 +1,69 @@
-public class Empregado{
+public class Empregado {
+    private String nome;
+    private int idade;
     private int tipo;
     private double salario;
     private double comissao;
     private double bonus;
-    private String nome;
-    private int idade;
-    sd
 
-    public double calcularSalario(){
-        switch(tipo){
-            case 1:
-                return salario;
-            case 2:
-                return salario + salario * comissao;
-            case 3:
-                return salario + bonus;
-            default:
-                return 0;
+    public double calculaSalario() {
+        if (tipo == 1) {
+            return salario;
+        } else if (tipo == 2) {
+            // comissão varia de 0 a 1, é um percentual sobre o salário
+            return salario + salario * comissao;
+        } else if (tipo == 3) {
+            return salario + bonus;
+        } else {
+            return 0.;
         }
     }
 
     public String getNome() {
-        return nome;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
+        return this.nome;
     }
 
     public void setNome(String nome) {
         this.nome = nome;
     }
 
+    public int getIdade() {
+        return this.idade;
+    }
 
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public void setTipo(int tipo) {
+        this.tipo = tipo;
+    }
+
+    public int tipo() {
+        return this.tipo;
+    }
+
+    public double getSalario() {
+        return this.salario;
+    }
+
+    public void setComissao(double comissao) {
+        this.comissao = comissao;
+    }
+
+    public double getComissao() {
+        return this.comissao;
+    }
+
+    public void setBonus(double bonus) {
+        this.bonus = bonus;
+    }
+
+    public double getBonus() {
+        return this.bonus;
+    }
 }
